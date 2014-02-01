8dc4ac4542a8915e2d379e98c953783462b45110
martinbomio <martinbomio@gmail.com>
2014-02-01 18:24:10 -0200

    change receiver

--- mapps-receiver/src/main/java/com.mapps.receiver/XBeeReceiver.java
+++ mapps-receiver/src/main/java/com.mapps.receiver/XBeeReceiver.java
@@ -3,7 +3,6 @@ package com.mapps.receiver;
 import org.apache.log4j.Logger;
 
 import com.rapplogic.xbee.api.ApiId;
-import com.rapplogic.xbee.api.PacketListener;
 import com.rapplogic.xbee.api.XBee;
 import com.rapplogic.xbee.api.XBeeException;
 import com.rapplogic.xbee.api.XBeeResponse;
@@ -20,42 +19,29 @@ public class XBeeReceiver {
         try {
             xbee.open("COM6", 115200);
             while (true) {
-                xbee.addPacketListener(new PacketListener() {
-                    @Override
-                    public void processResponse(XBeeResponse xBeeResponse) {
-                        if (xBeeResponse.getApiId() == ApiId.ZNET_RX_RESPONSE) {
-                            XbeeResponseDecoder decoder = new XbeeResponseDecoder((ZNetRxResponse) xBeeResponse);
-                            if (decoder.isPacketComplete()) {
-                                PacketBuilderPool pool = PacketBuilderPool.getDefaultInstace();
-                                String dir = decoder.getDir();
-                                if (!pool.hasPacketBuilder(dir)) {
-                                    PacketBuilder builder = new PacketBuilder(dir);
-                                    builder.setDirLow(dir);
-                                    pool.putPacketBuilder(dir, builder);
-                                }
-                                PacketBuilder builder = pool.getPacketBuilder(dir);
-                                builder.addPacket(decoder.getPayload());
-                            } else {
-                                logger.debug("The XBee packet is incomplete");
-                            }
+                XBeeResponse response = xbee.getResponse();
+                if (response.getApiId() == ApiId.ZNET_RX_RESPONSE) {
+                    XbeeResponseDecoder decoder = new XbeeResponseDecoder((ZNetRxResponse) response);
+                    if (decoder.isPacketComplete()) {
+                        PacketBuilderPool pool = PacketBuilderPool.getDefaultInstace();
+                        String dir = decoder.getDir();
+                        if (!pool.hasPacketBuilder(dir)) {
+                            PacketBuilder builder = new PacketBuilder(dir);
+                            builder.setDirLow(dir);
+                            pool.putPacketBuilder(dir, builder);
                         }
-                        logger.error("The XBee packet format is wrong");
+                        PacketBuilder builder = pool.getPacketBuilder(dir);
+                        builder.addPacket(decoder.getPayload());
+                    } else {
+                        logger.debug("The XBee packet is incomplete");
                     }
-                });
+                }
+                logger.error("The XBee packet format is wrong");
             }
-        } catch (XBeeException e) {
+        } catch (XBeeException e){
             e.printStackTrace();
         } finally {
             xbee.close();
         }
-    }
-
-    private static String getStringDir(String hex) {
-        String[] split = hex.split("0x");
-        StringBuilder dir = new StringBuilder();
-        for (String s : split) {
-            dir.append(s);
-        }
-        return dir.toString();
     }
 }
